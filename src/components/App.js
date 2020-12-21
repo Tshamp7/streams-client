b0a395e0230a1ef18e7ba5232c3636d52d814e5e
Thomas Shamp <T.shamp7@gmail.com>
2020-12-21 08:06:38 -0800

    Scaffold stream componenets

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const pageOne = () => {
-  return <div>pageOne</div>;
-};
-
-const pageTwo = () => {
-  return (
-    <div>
-      pageTwo
-      <button>Click Me!</button>
-    </div>
-  );
-};
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={pageOne} />
-          <Route path="/pagetwo" component={pageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/create" exact component={StreamCreate} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
         </div>
       </BrowserRouter>
     </div>
