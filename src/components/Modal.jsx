28cb6177c6478db10d0984f0a75349a05dc9c464
Thomas Shamp <T.shamp7@gmail.com>
2021-01-15 13:16:20 -0800

    implement modal inside StreamDelete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const Modal = (props) => {
-  return <div></div>;
+  return ReactDom.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">Stuff inside modal</div>
+    </div>,
+    document.querySelector("#modal")
+  );
 };
 
 export default Modal;
