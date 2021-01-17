a94dd593e28b0936847b4227e8d5e591a71a7045
Thomas Shamp <T.shamp7@gmail.com>
2021-01-17 10:29:44 -0800

    connect StreamDelete delete functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDom.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Stuff inside modal</div>
+    <div
+      onClick={() => history.push(props.route)}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.header}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
--- src/components/streams/StreamDelete.jsx
+++ src/components/streams/StreamDelete.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+import { deleteStream } from "../../actions";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
 
-const StreamDelete = () => {
+const StreamDelete = (props) => {
+  const actions = (
+    <div>
+      <button
+        className="ui button negative"
+        onClick={() => props.deleteStream(props.match.params.id)}
+      >
+        Delete
+      </button>
+      <Link to="/" className="ui button">
+        Cancel
+      </Link>
+    </div>
+  );
+
   return (
     <div>
       Stream Delete
-      <Modal />
+      <Modal
+        header="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        route="/"
+        actions={actions}
+      />
     </div>
   );
 };
 
-export default StreamDelete;
+export default connect(null, { deleteStream })(StreamDelete);
